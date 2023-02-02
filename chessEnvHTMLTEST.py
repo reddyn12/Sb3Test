aa4096b2b544e3bc3edb2742789b4c27a337dc29
reddyn <nikidsniper@gmail.com>
2023-02-02 02:16:40 -0500

    should have render working

--- chessEnvHTMLTEST.py
+++ chessEnvHTMLTEST.py
@@ -5,8 +5,7 @@ import chess.svg
 import sys
 import time
 import matplotlib.pyplot as plt
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 import http.server
 import socketserver
 import webbrowser
@@ -19,7 +18,6 @@ class HTMLServer:
         self.port = port
         self.html_content = self.strBuild(chess.svg.board(chess.Board()))
         self.httpd = None
-        self.driver = None
     def strBuild(self, s):
         ans = """
         <html>
@@ -31,7 +29,7 @@ class HTMLServer:
               }
             </script>
           </head>
-          <body>
+          <body onload="refresh();">
             """+s+"""
           </body>
         </html>
@@ -40,11 +38,14 @@ class HTMLServer:
         return ans
     class HTTPRequestHandlerWithContent(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
-            
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(self.server.html_content.encode())
+            if self.path == "/refresh":
+                self.send_response(200)
+                self.end_headers()
+            else:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(self.server.html_content.encode())
         
 
     def start(self):
@@ -52,21 +53,13 @@ class HTMLServer:
         self.httpd.html_content = self.html_content
         self.thread = threading.Thread(target=self.httpd.serve_forever)
         self.thread.start()
-        options = Options()
-        options.add_argument("disable-infobars")
-        self.driver = webdriver.Chrome(options=options)
-        self.driver.get(f"http://localhost:{self.port}")
-        self.driver.execute_script("document.body.style.pointerEvents = 'none';")
-        # webbrowser.open_new_tab(f"http://localhost:{self.port}")
+        webbrowser.open_new_tab(f"http://localhost:{self.port}")
     
     def stop(self):
-        
-        self.driver.quit()
-        self.driver.stop_client()
         self.httpd.shutdown()
+    
     def update(self, new_html_content):
         self.httpd.html_content = self.strBuild(new_html_content)
-        self.driver.refresh()
 
 
 
@@ -150,7 +143,7 @@ class ChessEnv(gym.Env):
         pass
     # chess.svg??
     def render(self):
-        self.html_server.driver.refresh()
+        pass
 
 
     # push uci for now for discrete action space... need to thnik if I want pgn or uci for final pipeline
@@ -159,7 +152,6 @@ class ChessEnv(gym.Env):
         reward = None
         done = None
         info = None
-        #not a valid moves
         try:
             self.board.push_uci(action)
         except Exception as e:
